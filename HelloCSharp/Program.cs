e58f546b0d076e90003108e31b145b06bd6dfbc6
dolsen0 <derek.olsen@gmail.com>
2022-10-18 20:46:23 -0400

    created based on Microsoft C# docs

---
+++ HelloCSharp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What's your name?");
+var name = Console.ReadLine();
+var currentDate = DateTime.Now;
+Console.WriteLine($"{Environment.NewLine} Hello {name}, Im your computer! it is {currentDate:t} on {currentDate:d}.");
+Console.Write($"{Environment.NewLine}Press any key to quit. ");
+Console.ReadKey(true);
